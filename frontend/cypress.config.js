c81af338793c53ccf1e73299e6f1ca5aba8413a4
AzeHie <109618459+AzeHie@users.noreply.github.com>
2024-03-21 18:23:42 +0200

    changed port to the cypress config

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:5000',
   },
   env: {
     BACKEND: 'http://localhost:3003/api'
